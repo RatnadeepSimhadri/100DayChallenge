cd06b9b1ee7c248b92010868fdef341cf1a84161
Ratnadeep <ratnadeep.ms@outlook.com>
2024-04-28 12:22:03 -0700

    Valid Palindrome II

---
+++ 04_28/LEETCODE/ValidPalindromeII.java
@@ -0,0 +1,29 @@
+
+/**
+ * Check if the Given String is a Palindrome after removing atmost 1 character
+ */
+class Solution {
+    public boolean validPalindrome(String s) {
+        int left = 0 , right = s.length() - 1;
+        while(left < right){
+            if(s.charAt(left) != s.charAt(right)){
+                return isPalindrome(s, left+1, right) || isPalindrome(s,left,right-1);
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+        
+    }
+
+
+    public boolean isPalindrome(String s, int left, int right){
+        while(left < right){
+            if(s.charAt(left) != s.charAt(right)) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
