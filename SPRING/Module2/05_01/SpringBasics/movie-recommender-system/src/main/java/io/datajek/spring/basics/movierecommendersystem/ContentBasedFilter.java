61fb7d137b7262c998ff8b3ec0d3086cb901b3a4
Ratnadeep Simhadri <rsimhadri@paypal.com>
2024-05-01 18:15:25 -0700

    Added Component, AutoWired and Primary Annotations

--- SPRING/Module2/05_01/SpringBasics/movie-recommender-system/src/main/java/io/datajek/spring/basics/movierecommendersystem/ContentBasedFilter.java
+++ SPRING/Module2/05_01/SpringBasics/movie-recommender-system/src/main/java/io/datajek/spring/basics/movierecommendersystem/ContentBasedFilter.java
@@ -1,5 +1,10 @@
 package io.datajek.spring.basics.movierecommendersystem;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
 public class ContentBasedFilter implements Filter {
     @Override
     public String[] getRecommendations(String movie) {
--- SPRING/Module2/05_01/SpringBasics/movie-recommender-system/src/main/java/io/datajek/spring/basics/movierecommendersystem/MovieRecommenderSystemApplication.java
+++ SPRING/Module2/05_01/SpringBasics/movie-recommender-system/src/main/java/io/datajek/spring/basics/movierecommendersystem/MovieRecommenderSystemApplication.java
@@ -4,21 +4,24 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+
 import java.util.Arrays;
 @SpringBootApplication
+/**
+ * @SpringBootApplication is an equivalent of 
+ * @Configuration, which declares a class as the source for bean definitions
+ * @EnableAutoConfiguration, which allows the application to add beans using classpath definitions
+ * @ComponentScan, which directs Spring to search for components in the path specified
+ */
 public class MovieRecommenderSystemApplication {
 	private static Logger logger = LoggerFactory.getLogger(MovieRecommenderSystemApplication.class);
 	public static void main(String[] args) {
-		Filter contentBasedFilter = new ContentBasedFilter();
-		Filter collabFilter = new CollaborativeFilter();
-		RecommenderImplementation recommender = new RecommenderImplementation(contentBasedFilter);	
-		RecommenderImplementation recommender2 = new RecommenderImplementation(collabFilter);	
+		ApplicationContext appContext = SpringApplication.run(MovieRecommenderSystemApplication.class, args);
 
-    	String[] result = recommender.recommendMovies("Finding Dory");
-		String[] result2 = recommender2.recommendMovies("Finding Dory");
-    	logger.info(Arrays.toString(result));
-		logger.info(Arrays.toString(result2));
-
+		RecommenderImplementation recommender = appContext.getBean(RecommenderImplementation.class);
+		logger.info(Arrays.toString(recommender.recommendMovies("Finding Dory")));
 	}
 
 }
