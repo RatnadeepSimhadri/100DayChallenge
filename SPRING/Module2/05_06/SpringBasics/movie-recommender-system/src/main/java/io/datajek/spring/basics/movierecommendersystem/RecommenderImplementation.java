2dc9847da5c89811c98fc7a17e5f4d8cc16f2967
Ratnadeep Simhadri <rsimhadri@paypal.com>
2024-05-06 16:53:39 -0700

    Autowiring by Name

--- SPRING/Module2/05_06/SpringBasics/movie-recommender-system/src/main/java/io/datajek/spring/basics/movierecommendersystem/RecommenderImplementation.java
+++ SPRING/Module2/05_06/SpringBasics/movie-recommender-system/src/main/java/io/datajek/spring/basics/movierecommendersystem/RecommenderImplementation.java
@@ -6,6 +6,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class RecommenderImplementation {
     
+    /**
+     * This is because @Autowired annotation tries to resolve dependency by type first. 
+     * If it fails to resolve a conflict and finds more than one bean of the same 
+     * type then it tries to resolve it by name.
+     */
     @Autowired
     private Filter contentBasedFilter;
 
