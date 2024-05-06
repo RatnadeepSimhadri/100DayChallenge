87fb2d6e0546117f99f5a4246997624ec3041a2e
Ratnadeep Simhadri <rsimhadri@paypal.com>
2024-05-06 16:23:35 -0700

    @primary annotation

--- SPRING/Module2/05_01/SpringBasics/movie-recommender-system/src/main/java/io/datajek/spring/basics/movierecommendersystem/CollaborativeFilter.java
+++ SPRING/Module2/05_01/SpringBasics/movie-recommender-system/src/main/java/io/datajek/spring/basics/movierecommendersystem/CollaborativeFilter.java
@@ -1,8 +1,10 @@
 package io.datajek.spring.basics.movierecommendersystem;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class CollaborativeFilter implements Filter {
     @Override
     public String[] getRecommendations(String movie) {
